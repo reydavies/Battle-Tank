ef92019a6e85db743d2123fd0fc994ddb116c17e
reydavies <reydavies@gmail.com>
2019-07-27 22:56:32 +0100

    Completed BattleTank

--- BattleTank/Source/BattleTank/Public/SprungWheel.h
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -31,6 +31,11 @@ protected:
 private:
 	void SetupConstraint();
 
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ApplyForce();
+
 	// Components
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USphereComponent* Wheel = nullptr;
@@ -43,5 +48,7 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPhysicsConstraintComponent* AxleWheelConstraint = nullptr;
+
+	float TotalForceMagnitudeThisFrame = 0;
 
 };
